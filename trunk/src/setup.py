5c40f007d19e0a6f2436205426af197aabdfa446
pierrejean.coudert <pierrejean.coudert@fd5a9aca-d834-0410-9fdf-291f3a001dc0>
2007-07-19 08:48:52 +0000

    0.5.1

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -30,15 +30,15 @@ setup(
   platforms=['any'],
   url='http://www.pwytter.com',
   keywords='twitter client python tkinter',
-#  packages=['twclient','simplejson'],
-#  package_dir={'twclient': 'twclient', 
-#               'simplejson': 'twclient:simplejson'},
-#  package_data={'twclient': ['doc/*.*']},
+  packages=['twclient','simplejson'],
+  package_dir={'twclient': 'twclient', 
+               'simplejson': 'twclient/simplejson'},
+  #package_data={'twclient': glob.glob('twclient/doc/*.*')},
   py_modules = ['pwytter','tkBalloon'],
   data_files=[("text", glob.glob("*.txt")),
               "pwytter.ico",
               ("media",glob.glob("media\\*.*"))],
-  #console=['pwytter.py'],
+  #console=['pwytter.py']
   windows = [
         {"script": "pwytter.py",
          "icon_resources": [(1, "pwytter.ico")]
