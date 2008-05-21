3e4384e945e7f9dd1e646a609048a09e4dcce232
pierrejean.coudert <pierrejean.coudert@fd5a9aca-d834-0410-9fdf-291f3a001dc0>
2008-05-21 08:59:51 +0000

    Norwegian translation updated

--- trunk/src/pwytter.py
+++ trunk/src/pwytter.py
@@ -114,7 +114,7 @@ class MainPanel(Frame):
                          "German": {"locale":"de_DE", "flag":"de.gif"},
                          "Italian": {"locale":"it_IT", "flag":"it.gif"},
                          "Japanese": {"locale":"ja_JP", "flag":"jp.gif"},
-                         "Norvegian": {"locale":"no_NO", "flag":"no.gif"},
+                         "Norwegian": {"locale":"no_NO", "flag":"no.gif"},
                          "Polish": {"locale":"pl_PL", "flag":"pl.gif"},
                          "Portuguese": {"locale":"pt_BR", "flag":"br.gif"},
                          "Romanian": {"locale":"ro_RO", "flag":"ro.gif"},
