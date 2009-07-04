ef7d4bbe4b934d83fe1493e749670264af1f949c
gogtesuyash <gogtesuyash@fd5a9aca-d834-0410-9fdf-291f3a001dc0>
2009-07-04 06:41:01 +0000

    *GUI* for notifications,whitelist maintained...

---
+++ src/pwNotifyParams.py
@@ -0,0 +1,52 @@
+import xml.dom.minidom as dom
+import os.path
+
+
+class PwytterNotifications(object):
+    """Handle the Pwtytter Notifications in an XML file Notifications.xml
+    """
+    def __init__(self,api):
+        self.api=api
+        self.storage_notification_directory="~/.pwytter/Notifications"
+        self.storageNotifications=os.path.expanduser(self.storage_notification_directory)
+        if not os.path.exists(self.storageNotifications):
+            os.makedirs(self.storageNotifications)
+        self._notificationFileName = os.path.join(self.storageNotifications,'notification.xml')
+        self.values={}
+        self._resetDefaults()
+
+    def _resetDefaults(self):
+        self.Friends=self.api.GetFriends()
+        for frnd in self.Friends :
+            self.values[frnd.screen_name]='1'
+
+    def __getitem__(self, aKey):
+        return self.values[aKey]
+
+    def __setitem__(self, aKey, value):
+        self.values[aKey] = value
+
+    def readFromXML(self):
+        self._resetDefaults()
+        self._notifyDoc = dom.parse(self._notificationFileName).documentElement
+        assert self._notifyDoc.tagName == 'notifications'
+        for val in self.values.keys():
+            try :
+                node=self._notifyDoc.getElementsByTagName(val)
+                self.values[val]=node[0].firstChild.data.strip()
+            except Exception, e:
+                print '!! Exception in process_node_string'+str(e)
+                #self.values[val]=''
+        print self.values
+
+    def writeToXML(self):
+        impl = dom.getDOMImplementation()
+        self._notifyDoc = impl.createDocument(None, 'notifications', None)
+        top_element = self._notifyDoc.documentElement
+        for val in self.values.keys():
+            Element=self._notifyDoc.createElement(val)
+            Element.appendChild(self._notifyDoc.createTextNode(str(self.values[val])))
+            top_element.appendChild(Element)
+        f=open(self._notificationFileName, 'w')
+        f.write(self._notifyDoc.toprettyxml())
+        f.close()
