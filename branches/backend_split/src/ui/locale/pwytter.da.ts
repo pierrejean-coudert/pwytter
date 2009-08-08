012c8a87cf815f66027675c607054e38e63c9e9e
jopsen <jopsen@fd5a9aca-d834-0410-9fdf-291f3a001dc0>
2009-08-08 12:10:01 +0000

    * build.py: Rules for building translations files and a ressource file for all translations. 	* pwytter.pyw: Loads translation ressources and uses translations...

---
+++ branches/backend_split/src/ui/locale/pwytter.da.ts
@@ -0,0 +1,301 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="da_DK">
+<defaultcodec></defaultcodec>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui_mainwindow.py" line="189"/>
+        <source>Pwytter</source>
+        <translation>Pwytter</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="190"/>
+        <source>&lt;b&gt;Reply to&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Svar til&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="191"/>
+        <source>Clear reply/direct message settings for this message.</source>
+        <translation>Ryd svar/direkte besked instillinger for denne besked.</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="192"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="193"/>
+        <source>Update</source>
+        <translation>Opdater</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="194"/>
+        <source>Account to post status update to.</source>
+        <translation>Konto som status opdatering skal sendes til.</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="195"/>
+        <source>&amp;File</source>
+        <translation>&amp;Filer</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="196"/>
+        <source>&amp;View</source>
+        <translation>&amp;Vis</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="197"/>
+        <source>&amp;Accounts</source>
+        <translation>&amp;Kontoer</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="198"/>
+        <source>Add account</source>
+        <translation>Tilføj konto</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="199"/>
+        <source>Remove account</source>
+        <translation>Fjern konto</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="206"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjælp</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="201"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Rediger</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="202"/>
+        <source>Content toolbar</source>
+        <translation>Indholds værktøjslinje</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="203"/>
+        <source>&amp;Twitter account</source>
+        <translation>&amp;Twitter konto</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="204"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Afslut</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="205"/>
+        <source>&amp;About</source>
+        <translation>&amp;Om</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="207"/>
+        <source>&amp;Synchronize accounts</source>
+        <translation>&amp;Synkroniser kontoer</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="208"/>
+        <source>&amp;Identi.ca account</source>
+        <translation>&amp;Identi.ca konto</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="209"/>
+        <source>&amp;Hide window</source>
+        <translation>&amp;Skjul vindue</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="210"/>
+        <source>Toggle new message field.</source>
+        <translation>&amp;Vis/skjul ny besked felt.</translation>
+    </message>
+    <message>
+        <location filename="ui_mainwindow.py" line="211"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Indstillinger</translation>
+    </message>
+</context>
+<context>
+    <name>NewIdenticaAccountDialog</name>
+    <message>
+        <location filename="ui_newidenticaaccountdialog.py" line="64"/>
+        <source>Connect to Identi.ca</source>
+        <translation>Forbind til Identi.ca</translation>
+    </message>
+    <message>
+        <location filename="ui_newidenticaaccountdialog.py" line="65"/>
+        <source>Connect to an existing Identi.ca. You will be able to synchronize and post updates using this account in pwytter.</source>
+        <translation>Forbind til en eksisterende Identi.ca konto. Du kan synkronisere og sende opdateringer med denne konto i Pwytter.</translation>
+    </message>
+    <message>
+        <location filename="ui_newidenticaaccountdialog.py" line="66"/>
+        <source>Identi.ca login</source>
+        <translation>Identi.ca login</translation>
+    </message>
+    <message>
+        <location filename="ui_newidenticaaccountdialog.py" line="67"/>
+        <source>Username:</source>
+        <translation>Brugernavn:</translation>
+    </message>
+    <message>
+        <location filename="ui_newidenticaaccountdialog.py" line="68"/>
+        <source>Password:</source>
+        <translation>Kodeord:</translation>
+    </message>
+</context>
+<context>
+    <name>NewTwitterAccountDialog</name>
+    <message>
+        <location filename="ui_newtwitteraccountdialog.py" line="64"/>
+        <source>Connect to Twitter</source>
+        <translation>Forbind til Twitter</translation>
+    </message>
+    <message>
+        <location filename="ui_newtwitteraccountdialog.py" line="65"/>
+        <source>Connect to an existing Twitter account. You will be able to synchronize and post updates using this account in pwytter.</source>
+        <translation>Forbind til en eksisterende Twitter konto. Du kan synkronisere og sende opdateringer til denne konto med Pwytter.</translation>
+    </message>
+    <message>
+        <location filename="ui_newtwitteraccountdialog.py" line="66"/>
+        <source>Twitter login</source>
+        <translation>Twitter login</translation>
+    </message>
+    <message>
+        <location filename="ui_newtwitteraccountdialog.py" line="67"/>
+        <source>Username:</source>
+        <translation>Brugernavn:</translation>
+    </message>
+    <message>
+        <location filename="ui_newtwitteraccountdialog.py" line="68"/>
+        <source>Password</source>
+        <translation>Kodeord:</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="233"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="234"/>
+        <source>Synchronization interval:</source>
+        <translation>Synkroniserings interval:</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="235"/>
+        <source>s</source>
+        <translation>s</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="236"/>
+        <source>Number of tweet per page:</source>
+        <translation>Antal beskeder per side:</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="237"/>
+        <source>Number of users per page:</source>
+        <translation>Antal bruger per side:</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="238"/>
+        <source>Show main window at startup.</source>
+        <translation>Vis hoved vindue ved opstart.</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="239"/>
+        <source>General</source>
+        <translation>Generalt</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="240"/>
+        <source>Notification on new tweet in timeline</source>
+        <translation>Underretning ved ny besked i tidslinje</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="241"/>
+        <source>Notification on new reply</source>
+        <translation>Underretning ved nyt svar</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="242"/>
+        <source>Notification on new direct message</source>
+        <translation>Underretning ved ny direkte besked</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="243"/>
+        <source>Notification on new follower</source>
+        <translation>Underretning ved ny følger</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="244"/>
+        <source>Notification on new friend</source>
+        <translation>Underretning ved ny ven</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="245"/>
+        <source>Notification on synchronization completed</source>
+        <translation>Underretning ved afsluttet synkronisering</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="246"/>
+        <source>Blacklist users</source>
+        <translation>Blokerede bruger liste</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="247"/>
+        <source>Whitelisted users</source>
+        <translation>Positiv bruger liste</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="249"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="250"/>
+        <source>Blacklisted users</source>
+        <translation>Blockerede bruger liste</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="251"/>
+        <source>Blacklisted tags</source>
+        <translation>Blokerede tag liste</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="254"/>
+        <source>Add</source>
+        <translation>Tilføj</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="253"/>
+        <source>Whitelisted tags</source>
+        <translation>Positive tag liste</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="255"/>
+        <source>Notifications</source>
+        <translation>Underretninger</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="256"/>
+        <source>Theme information</source>
+        <translation>Tema information</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="257"/>
+        <source>preview</source>
+        <translation>Forhåndsvisning</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="258"/>
+        <source>Select theme</source>
+        <translation>Vælg tema</translation>
+    </message>
+    <message>
+        <location filename="ui_preferencesdialog.py" line="259"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+</context>
+</TS>
