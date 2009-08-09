b264139647755dbce189ada58761e1520899c756
gogtesuyash <gogtesuyash@fd5a9aca-d834-0410-9fdf-291f3a001dc0>
2009-08-09 13:50:03 +0000

    XMPP client basic version for pwytter

---
+++ branches/backend_split/pwytterIM/photo.py
@@ -0,0 +1,64 @@
+import base64
+import os
+import sha
+import xmpp
+
+PHOTO_DIR = "./photos/"
+
+PHOTO_TYPES = {
+    'image/png': '.png',
+    'image/jpeg': '.jpeg',
+    'image/gif': '.gif',
+    'image/bmp': '.bmp',
+    'image/jpg':'.jpg',
+	}
+
+def append_directory(filename):
+    return os.path.join(PHOTO_DIR, filename)
+
+def register_handler(session):
+    session.RegisterHandler('presence', photo_update_handler)
+
+def photo_update_handler(session, stanza):
+    JID = stanza['from'].getStripped()
+    vupdate = stanza.getTag('x', namespace='vcard-temp:x:update')
+    if not vupdate:
+        return
+    photo = vupdate.getTag('photo')
+    if not photo:
+        return
+    photo = photo.getData()
+    if not photo:
+        return
+    #request the photo only if we don't have it already
+    if not get_photo(photo):
+        try:
+		request_vcard(session, JID)
+	except Exception,e :
+		print "123",e
+def get_photo(photo_hash):
+    for ext in PHOTO_TYPES.values():
+        filepath = append_directory(photo_hash + ext)
+        if os.path.exists(filepath):
+            return filepath
+
+def request_vcard(session, JID):
+    n = xmpp.Node('vCard', attrs={'xmlns': xmpp.NS_VCARD})
+    iq = xmpp.Protocol('iq', JID, 'get', payload=[n])
+    return session.SendAndCallForResponse(iq, recieve_vcard)
+
+def recieve_vcard(session, stanza):
+    photo = stanza.getTag('vCard').getTag('PHOTO')
+    if not photo:
+        return
+    photo_type = photo.getTag('TYPE').getData()
+    photo_bin = photo.getTag('BINVAL').getData()
+    photo_bin = base64.b64decode(photo_bin)
+    ext = PHOTO_TYPES[photo_type]
+    photo_hash = sha.new()
+    photo_hash.update(photo_bin)
+    photo_hash = photo_hash.hexdigest()
+    #name=stanza['from'].getStripped()
+    filename = append_directory(photo_hash + ext)
+    file(filename, 'wb').write(photo_bin)
+
